0ba4f07f1cb5d42569fb0721e05f0f7d5e5a1f6d
Hank <hank.butler@protonmail.com>
2022-06-27 19:35:36 -0500

    create link_list.py

---
+++ data_structures/link_list.py
@@ -0,0 +1,21 @@
+'''
+===========
+LINKED LIST
+===========
+
+Linking together nodes using next_node property craetes a singly
+linked link.
+Extremely versatile and useful.
+Used as foundations for future data structures.
+
+'''
+
+'''
+===================
+Doubly Linked Lists
+===================
+
+
+
+
+'''
