24652ec24c434d3da8c05335d013110c71360c27
Hank <hank.butler@protonmail.com>
2022-06-27 19:39:01 -0500

    added descriptions to comments on queues, stacks

--- data_structures/link_list.py
+++ data_structures/link_list.py
@@ -16,6 +16,39 @@ Doubly Linked Lists
 ===================
 
 
+Single linked list consists of nodes with links from one node to the next.
+A doubly linked list has a link to the node before it.
+prev_node links, along with the added tail_nail property, allow you to iterate
+backward through the list as easily as you could iterate forward singly linked list.
+
+'''
 
 
 '''
+======
+Queues
+======
+A linear collection of nodes that exclusively asdds nodes to the tail
+and removes nodes from the head of the queue.
+
+Implemented using different underlying data structures, but more common method
+is a singly linked list.
+
+Think of like a line in a grocery store.
+'''
+
+'''
+======
+Stacks
+======
+
+Like a queue, a stack is a linear collection of nodes that adds (pushes) data to the head
+of the stack. However, a stack removes data from the head of the stack. Think of like a stack of books.
+
+
+
+
+'''
+
+import os
+print(f' This is the current working directory: {os.getcwd()}')
