a8bbadcf6bafa4159436aa27d7441ac2bbfce3a5
Hank <hank.butler@protonmail.com>
2022-06-28 22:16:54 -0500

    added sum1 function

--- data_structures/python_algo_ds_interview_course/big_o.py
+++ data_structures/python_algo_ds_interview_course/big_o.py
@@ -4,3 +4,15 @@ Start off assuming we don't know Big-O notation.
 algorithm is simply a procedure or formula for solving a problem!
 
 '''
+
+# First function
+def sum1(n):
+    '''
+    Take an input of n and return the sum of the numbers from 0 to n
+
+    '''
+    final_sum = 0
+    for x in range(n+1):
+        final_sum += x
+
+    return final_sum
