1a6dcaebf76c0b74c51f585769e719fe272f8d15
Hank <hank.butler@protonmail.com>
2022-07-06 13:16:16 -0500

    update linklist

--- data_structures/link_list.py
+++ data_structures/link_list.py
@@ -49,3 +49,25 @@ of the stack. However, a stack removes data from the head of the stack. Think of
 
 import os
 print(f' This is the current working directory: {os.getcwd()}')
+
+# We'll be using our Node class
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+
+  def get_value(self):
+    return self.value
+
+  def get_next_node(self):
+    return self.next_node
+
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+
+# Create your LinkedList class below:
+class LinkedList:
+  def __init__(self, value = None):
+    self.head_node = Node(value)
+  def get_head_node(self):
+    return self.head_node
