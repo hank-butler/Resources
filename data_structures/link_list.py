de1d94c8a161dee6ae4c0571058c2eea171f8bfc
Hank <hank.butler@protonmail.com>
2022-07-06 13:51:44 -0500

    deleted duplicative class

--- data_structures/link_list.py
+++ data_structures/link_list.py
@@ -67,17 +67,12 @@ class Node:
 
 # Create your LinkedList class below:
 class LinkedList:
-  def __init__(self, value = None):
-    self.head_node = Node(value)
-  def get_head_node(self):
-    return self.head_node
-class LinkedList:
   def __init__(self, value=None):
     self.head_node = Node(value)
 
   def get_head_node(self):
     return self.head_node
-  
+
   def insert_beginning(self, new_value):
     new_node = Node(new_value)
     new_node.set_next_node(self.head_node)
