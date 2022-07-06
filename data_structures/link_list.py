691901cad6c13c7a7da235cf8c4b8c6f588f285f
Hank <hank.butler@protonmail.com>
2022-07-06 13:51:24 -0500

    added insert_beginning function to linked list class

--- data_structures/link_list.py
+++ data_structures/link_list.py
@@ -71,3 +71,14 @@ class LinkedList:
     self.head_node = Node(value)
   def get_head_node(self):
     return self.head_node
+class LinkedList:
+  def __init__(self, value=None):
+    self.head_node = Node(value)
+
+  def get_head_node(self):
+    return self.head_node
+  
+  def insert_beginning(self, new_value):
+    new_node = Node(new_value)
+    new_node.set_next_node(self.head_node)
+    self.head_node = new_node
