531cc5c96b7f948c8c6d8ceb0667162684ae4f47
Hank <hank.butler@protonmail.com>
2022-07-21 11:43:47 -0500

    conceptual material inside link_list

--- data_structures/link_list.py
+++ data_structures/link_list.py
@@ -168,6 +168,17 @@ We will set the next pointer of the new tail (the element directly before the ta
  to null, and update the tail property of the list.
  If the tail was also the head, the head removal process will occur as well.
 
+====================================
+Removing from the Middle of the List
+====================================
+It is also possible to remove a node from the middle of the list.
+Since that node is neither the head nor the tail of the list,
+there are two pointers that must be updated:
 
+We must set the removed node’s preceding node’s next pointer to its following node
+We must set the removed node’s following node’s previous pointer to its preceding node
+There is no need to change the pointers of the removed node,
+as updating the pointers of its neighboring nodes will remove it from the list.
+If no nodes in the list are pointing to it, the node is orphaned.
 
 '''
